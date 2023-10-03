894c8cff6c0cdf6a561b80d3be2ec6bb5a2ef4e1
MatinxHT <linduanpeng@live.com>
2023-10-03 23:17:33 +0800

    add graph

---
+++ DataClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIEChromaticityDiagram
+{
+    public class DataClass
+    {
+        public struct DataGridValue
+        {
+            public byte address;
+            public byte Channel;
+            public double ciex;
+            public double ciey;
+            public double cieu;
+            public double ciev;
+            public string? TestTime;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # An .Net CIE Chromaticity Diagram Drawing Program(WPF)
 
+## Introduction
+a sample code for ChromaticityDotNet and about:
+- show CIE xy Chromaticity Grap
+- CIE Color cover
+
+## What's more
 Updating...
