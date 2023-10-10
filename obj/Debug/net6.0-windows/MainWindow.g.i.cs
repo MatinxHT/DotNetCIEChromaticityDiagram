17e6d762cab62bc202b84b151569fb0f40e56f3f
MatinxHT <linduanpeng@live.com>
2023-10-10 17:01:25 +0800

    fixed file system

--- obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace CIEChromaticityDiagram {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CIEChromaticityDiagram;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CIEChromaticityDiagram;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
